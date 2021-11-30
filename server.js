6e446b94403fbfc5c600dddb1cf05c914c1bbefa
experiencei <ayelojahighbee01@gmail.com>
2021-11-30 17:31:26 +0100

    using cluster to enhance performance

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cluster = require('cluster')
+const os = require('os')
 
 const app = express();
 
@@ -21,8 +22,12 @@ app.get('/timer', (req, res) => {
 })
 
 if(cluster.isMaster){
-   cluster.fork()
-   cluster.fork()
+  const NUM_WORKERS = os.cpus().length;
+  
+  for (let i = 0; i < NUM_WORKERS ; i++) {
+    cluster.fork()
+  }
+  
 }else{
     app.listen(3000 , ()=> {
         console.log("working on port 3000");
