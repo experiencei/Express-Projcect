4296076a8e8ff927016799a0b1113e17bb594b3b
experiencei <ayelojahighbee01@gmail.com>
2021-11-30 16:10:56 +0100

    solving performance example with express in node

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express();
 
+function delay(duration){
+     const start = new Date();
+     while (Date.now() - start < duration) {
+
+     }
+}
+
+app.get('/', (req, res) => {
+    res.send("perfomance Example")
+});
 
 
-app.listen("/" , ()=> {
-
+app.get('/timer', (req, res) => {
+    delay(9000)
+    res.send("Ding Dong")
+})
+app.listen(3000 , ()=> {
+  console.log("working on port 3000");
 })
